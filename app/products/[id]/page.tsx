030f5090a0bf98cb1e5d1a63ecd0ca66e4b044c2
Diones <pereiradiones987@gmail.com>
2024-05-02 22:16:51 -0300

    feat: adicionando tela de detalhes

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,52 @@
+import { db } from "@/app/lib/prisma";
+import { notFound } from "next/navigation";
+import ProductImage from "./components/product-image";
+import ProductDetails from "./components/product-details";
+
+interface ProductsPageProps {
+  params: {
+    id: string
+  }
+}
+
+const ProductsPage = async ({ params: { id } }: ProductsPageProps) => {
+
+  const product = await db.product.findUnique({
+    where: {
+      id
+    },
+    include: {
+      restaurant: true,
+      category: true
+    }
+  });
+
+  
+
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: product?.category.name 
+      }
+    },
+    include: {
+      restaurant: true
+    }
+  })
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <>
+      <div>
+        <ProductImage product={product}/>
+
+        <ProductDetails product={product} complementaryProducts={juices} />
+      </div>
+    </>
+  );
+}
+ 
+export default ProductsPage;
